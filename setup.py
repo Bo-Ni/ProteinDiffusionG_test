60fdfa5bbd15e813dc3a64e6d7d74a7ada4bfdfb
Bo-Ni <boni.mechanics@gmail.com>
2023-04-18 17:17:58 -0400

    test step 2

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from my_pip_package import __version__
+
+setup(
+    name='my_pip_package',
+    version=__version__,
+
+    url='https://github.com/Bo-Ni/ProteinDiffusionG_test',
+    author='Bo Ni',
+    author_email='boni.mechanics@gmail.com',
+
+    py_modules=['my_pip_package'],
+)
